f675f6bd6d55940b7bc641e4f7defc6fe99b635e
Sumanta Parida <sumanta90parida@gmail.com>
2023-05-31 21:53:44 +0530

    commit - 02

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 import type { ReactNode } from 'react';
 
 import { AppConfig } from '@/utils/AppConfig';
@@ -9,35 +9,15 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => (
-  <div className="w-full text-gray-700 antialiased">
+  <div className="w-full flex flex-col">
     {props.meta}
-
-    <div className="mx-auto max-w-screen-lg">
-      <header className="border-b border-gray-300">
-        <div className="pb-8 pt-16">
-          <h1 className="text-3xl font-bold text-gray-900">
-            {AppConfig.title}
-          </h1>
-          <h2 className="text-xl">{AppConfig.description}</h2>
-        </div>
-        <nav>
-          <ul className="flex flex-wrap text-xl">
-            <li className="mr-6">
-              <Link
-                href="/"
-                className="border-none text-gray-700 hover:text-gray-900"
-              >
-                Home
-              </Link>
-            </li>
-          </ul>
-        </nav>
+    <div className="">
+      <header className="border-b border-gray-300 bg-teal-950 py-2">
+        {AppConfig.title}
       </header>
-
       <main className="content py-5 text-xl">{props.children}</main>
-
-      <footer className="border-t border-gray-300 py-8 text-center text-sm">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
+      <footer className="border-t border-gray-300 bg-teal-950 py-8 text-center text-sm">
+        © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{" "}
         <a href="#">Anas,Swapnil,Shudhansu</a>.
       </footer>
     </div>
